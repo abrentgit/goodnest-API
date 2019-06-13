34ca2f7241ca52bc662de27206344f75a155a2ba
abrentgit <abprojects.ca@gmail.com>
2019-06-12 20:28:57 -0400

    updated server to run

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const { CLIENT_ORIGIN } = require('./config');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const { PORT, DATABASE_URL } = require('./config');
-const { Restaurant } = require('./models');
 
 app.use(
   cors({
@@ -13,12 +12,52 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 3000;
-
-app.get('/api/*', (req, res) => {
-  res.json({ ok: true });
+app.get('/', (req, res) => {
+  const message = 'Hello world';
+  res.send(message);
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
 
-module.exports = { app };
+// let server;
+
+// function runServer(databaseUrl, port = PORT) {
+//   return new Promise((resolve, reject) => {
+//     mongoose.connect(databaseUrl, err => {
+//       if (err) {
+//         return reject(err);
+//       }
+//       server = app
+//         .listen(port, () => {
+//           console.log(`Your app is listening on port ${port}`);
+//           resolve();
+//         })
+//         .on('error', err => {
+//           mongoose.disconnect();
+//           reject(err);
+//         });
+//     });
+//   });
+// }
+
+// function closeServer() {
+//   return mongoose.disconnect().then(() => {
+//     return new Promise((resolve, reject) => {
+//       console.log('Closing server');
+//       server.close(err => {
+//         if (err) {
+//           return reject(err);
+//         }
+//         resolve();
+//       });
+//     });
+//   });
+// }
+
+// if (require.main === module) {
+//   runServer(DATABASE_URL).catch(err => console.error(err));
+// }
+
+// module.exports = { runServer, app, closeServer };
