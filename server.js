e31dbd06e0765accc069afdb83e0357b51d857fb
abrentgit <abprojects.ca@gmail.com>
2019-06-16 01:08:28 -0400

    login endpoint working

--- server.js
+++ server.js
@@ -63,7 +63,57 @@ app.post('/register', (req, res) => {
     });
 });
 
+const createAuthToken = function(user) {
+  return jwt.sign({ user }, config.JWT_SECRET, {
+    subject: user.email,
+    audience: user.role,
+    expiresIn: config.JWT_EXPIRY,
+    algorithm: 'HS256'
+  });
+};
+
 // LOGIN USER
+
+app.post('/login', (req, res) => {
+  User.findOne(
+    {
+      email: req.body.email
+    },
+    function(err, user) {
+      console.log('error', err);
+      console.log('user', user);
+      console.log(req.body.email);
+      if (err) {
+        res.status(401).json({
+          error: 'Invalid credentials'
+        });
+      }
+
+      if (!user) {
+        res.status(404).json({
+          error: 'Invalid credentials'
+        });
+      } else {
+        let validPassword = bcrypt.compareSync(
+          req.body.password,
+          user.password
+        );
+
+        if (!validPassword) {
+          res.status(401).json({
+            error: 'Invalid credentials'
+          });
+        } else {
+          const authToken = createAuthToken(user.serialize());
+          res.status(200).json({
+            authToken,
+            user_id: user._id
+          });
+        }
+      }
+    }
+  );
+});
 
 let server;
 
